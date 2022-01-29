0225a6333303097522e420ce344457125cbf9339
Contra-KSA <46282006+Contra-KSA@users.noreply.github.com>
2022-01-29 19:47:50 +0300

    Create App.controller.js

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  function(Controller) {
+    "use strict";
+
+    return Controller.extend("companyRepo.appName.controller.App", {
+      //Old way of metadata initialization. Currently: define backend service in two sections of manifest.json file
+      // onInit: function() {
+      // var me = this;
+      // me.getView().setBusy(true);
+      // var oModel = new sap.ui.model.odata.v2.ODataModel({
+      //   serviceUrl: "/Northwind/Northwind.svc",
+      //   //          serviceUrl: "/sap/opu/odata/backendServerPath",
+      // });
+      // oModel.metadataLoaded().then(function(oEvent) {
+      //   console.log(oEvent);
+      //   //sap default model has no name!
+      //   me.getView().setModel(oModel);
+      //   //me.getView().setModel(oModel, "northwind");
+      //   me.getView().setBusy(false);
+      // });
+      // },
+    });
+  }
+);
