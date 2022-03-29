999c7095c197f33bf5fedcd89e89dd57479bacad
Sergey Kondrashov <Sergey.Kondrashov@kofax.com>
2022-03-29 09:06:35 +0300

    all done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller"],
-  function(Controller) {
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function(Controller,
+	JSONModel) {
     "use strict";
 
     return Controller.extend("companyRepo.appName.controller.App", {
+      onInit: function() {},
+      onPress: function(event) {
+        var text = event.getSource().getText();
+        alert(text + " Button was pressed");
+      },
       //Old way of metadata initialization. Currently: define backend service in two sections of manifest.json file
       // onInit: function() {
       // var me = this;
